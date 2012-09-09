b13751ff13ceb04188877d64631ff5ef6a52bafd
Sriram Narasimhan <narasis@thoughtworks.com>
2012-09-09 08:28:38 -0700

    Sriram:Completing past worldcups feature

--- ICCWorldT20/PastWCViewController.h
+++ ICCWorldT20/PastWCViewController.h
@@ -3,9 +3,19 @@
 @interface PastWCViewController : BaseAppController {
     NSString *pastYear;
     IBOutlet UILabel *header;
+    IBOutlet UILabel *result;
+    IBOutlet UITextView *mostRuns;
+    IBOutlet UITextView *mostSixers;
+    IBOutlet UITextView *mostWickets;
+    IBOutlet UITextView *mostCatches;
     IBOutlet UIImageView *winner;
 }
 @property (nonatomic, strong) NSString *pastYear;
 @property (nonatomic, strong) IBOutlet UILabel *header;
 @property (nonatomic, strong) IBOutlet UIImageView *winner;
+@property (nonatomic, strong) IBOutlet UILabel *result;
+@property (nonatomic, strong) IBOutlet UITextView *mostRuns;
+@property (nonatomic, strong) IBOutlet UITextView *mostSixers;
+@property (nonatomic, strong) IBOutlet UITextView *mostWickets;
+@property (nonatomic, strong) IBOutlet UITextView *mostCatches;
 @end
--- ICCWorldT20/PastWCViewController.m
+++ ICCWorldT20/PastWCViewController.m
@@ -1,7 +1,8 @@
 #import "PastWCViewController.h"
+#import "JSONUtil.h"
 
 @implementation PastWCViewController
-@synthesize pastYear,header,winner;
+@synthesize pastYear,header,winner,result,mostRuns,mostSixers,mostCatches,mostWickets;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -14,13 +15,31 @@
 
 #pragma mark - View lifecycle
 
-- (void)viewDidLoad {
-    NSString *headerText = [NSString stringWithFormat:@"%@ T20 WorldCup",pastYear];
+- (void)setImage {
     NSString *winnerImageName = [NSString stringWithFormat:@"/%@Winners.jpg",pastYear]; 
     NSString *fullpath = [[[NSBundle mainBundle] bundlePath] stringByAppendingString:winnerImageName];  
     UIImage *winnerImage = [UIImage imageWithContentsOfFile:fullpath];
-    [self.header setText:headerText];
     [self.winner setImage:winnerImage];
+}
+
+- (void)setHeader {
+    NSString *headerText = [NSString stringWithFormat:@"%@ T20 WorldCup",pastYear];
+    [self.header setText:headerText];
+}
+
+- (void)viewDidLoad {
+    [self setHeader];
+    [self setImage];
+    NSMutableDictionary *results = [JSONUtil loadJSON:@"results" mainKey:@"results"];
+    NSMutableDictionary *pastYearResult = [results objectForKey:pastYear];
+    [self.result setText:[pastYearResult objectForKey:@"result"]];
+
+    [self.mostRuns setText:[pastYearResult objectForKey:@"runs"]];
+    [self.mostWickets setText:[pastYearResult objectForKey:@"wickets"]];
+    [self.mostCatches setText:[pastYearResult objectForKey:@"catches"]];
+    [self.mostSixers setText:[pastYearResult objectForKey:@"sixers"]];
+    
+    
     [super viewDidLoad];
 }
 
