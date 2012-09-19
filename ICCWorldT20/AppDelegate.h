de21a2101dca6952911a49b1d38571402870809e
Sriram Narasimhan <narasis@thoughtworks.com>
2012-09-19 07:50:38 -0700

    Sriram : flushing git

--- ICCWorldT20/AppDelegate.h
+++ ICCWorldT20/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  ICCWorldT20
-//
-//  Created by Sriram Narasimhan on 02/09/12.
-//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
